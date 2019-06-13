fbdcccb7971b969c56bee21ccdea0debe818d025
jiangxuan <jiangxuan@dxy.cn>
2019-06-13 14:55:05 +0800

    enhance test

--- test/SearchList.test.tsx
+++ test/SearchList.test.tsx
@@ -5,6 +5,7 @@ import * as _ from 'lodash'
 import * as enzyme from 'enzyme';
 import { Location } from 'history';
 import SearchList from '@/SearchList';
+import moment from 'moment';
 
 /**
  * SearchList 组件依赖:
@@ -156,63 +157,146 @@ test('should: 从 location props 中正确的初始化参数, 含有特殊的 ti
   expect(testInstance.findByType(OriginComponent).props.urlQueryParams).toEqual(mockQuery);
 })
 
-test('should: 在点击搜索按钮的时候调用正确的函数和传入正确的参数', () => {
-  const mockQuery = {
-    id: '123456',
-  }
-  const dispatch = jest.fn(() => {})
-  const validateFields = jest.fn((callback) => {
-    callback(null, {
-      id: '123456',
-    })
-  })
-  const routerReplace = jest.fn(() => {})
-  const location = {
-    pathname: '/trending',
-    search: '',
-    state: '',
-    hash: '',
-    query: {
-      ...mockQuery
-    },
-  }
-  const Router = {
-    replace: routerReplace,
-    push: () => {},
-    go: () => {},
-    goBack: () =>{},
-  }
-
-  const compose = _.flowRight(
-    connect(dispatch, location, {
-      form: {
-        validateFields
-      }
-    }),
-    SearchList({
-      listRequestType: 'testDispatch/getList',
-      singleFieldPrefix: 'isAssoc',
-      singleFields: ['stdElement'],
-      DO_NOT_USE_THIS_OPTIONS_ROUTER: Router,
-    })
-  )
-
-  const Component = compose(OriginComponent)
-
-  const testRenderer = enzyme.mount(<Component />);
-
-  const originComponent = testRenderer.find(OriginComponent)
-  originComponent.find('.searchButton').simulate('click')
-
-  expect(validateFields).toBeCalled()
-  expect(routerReplace).toBeCalled()
-  expect(dispatch).toBeCalled()
-  expect(routerReplace).toBeCalledWith({
-    pathname: '/trending',
-    query: {
-      p: 1,
-      pageSize: 15,
+describe('should: 在点击搜索按钮的时候调用正确的函数和传入正确的参数', () => {
+  test('should: 只入参 singleFields, singleFieldPrefix. 正确的调用 dispatch, validateFields, Router.replace.', () => {
+    const mockQuery = {
       id: '123456',
     }
+    const dispatch = jest.fn(() => {})
+    const validateFields = jest.fn((callback) => {
+      callback(null, {
+        id: '123456',
+      })
+    })
+    const routerReplace = jest.fn(() => {})
+    const location = {
+      pathname: '/trending',
+      search: '',
+      state: '',
+      hash: '',
+      query: {
+        ...mockQuery
+      },
+    }
+    const Router = {
+      replace: routerReplace,
+      push: () => {},
+      go: () => {},
+      goBack: () =>{},
+    }
+  
+    const compose = _.flowRight(
+      connect(dispatch, location, {
+        form: {
+          validateFields
+        }
+      }),
+      SearchList({
+        listRequestType: 'testDispatch/getList',
+        singleFieldPrefix: 'isAssoc',
+        singleFields: ['stdElement'],
+        DO_NOT_USE_THIS_OPTIONS_ROUTER: Router,
+      })
+    )
+  
+    const Component = compose(OriginComponent)
+  
+    const testRenderer = enzyme.mount(<Component />);
+  
+    const originComponent = testRenderer.find(OriginComponent)
+    originComponent.find('.searchButton').simulate('click')
+  
+    expect(validateFields).toBeCalled()
+    expect(routerReplace).toBeCalled()
+    expect(routerReplace).toBeCalledWith({
+      pathname: '/trending',
+      query: {
+        p: 1,
+        pageSize: 15,
+        id: '123456',
+      }
+    })
+    expect(dispatch).toBeCalled()
+    expect(dispatch).toBeCalledWith({
+      type: 'testDispatch/getList',
+      payload: {
+        p: 1,
+        pageSize: 15,
+        id: '123456',
+      },
+    })
+  })
+
+  test('should: 只入参 timeRangeFields. 正确的调用 dispatch, validateFields, Router.replace', () => {
+    const mockQuery = {
+      id: '123456',
+    }
+    const dispatch = jest.fn(() => {});
+    const validateFields = jest.fn((callback) => {
+      callback(null, {
+        updateTime: [moment(1560407706310), moment(1560407857997)],
+      })
+    })
+    const routerReplace = jest.fn(() => {})
+    const location = {
+      pathname: '/trending',
+      search: '',
+      state: '',
+      hash: '',
+      query: {
+        ...mockQuery
+      },
+    }
+    const Router = {
+      replace: routerReplace,
+      push: () => {},
+      go: () => {},
+      goBack: () =>{},
+    }
+
+    const compose = _.flowRight(
+      connect(dispatch, location, {
+        form: {
+          validateFields
+        }
+      }),
+      SearchList({
+        listRequestType: 'testDispatch/getList',
+        timeRangeFields: ['updateTime', 'createTime'],
+        DO_NOT_USE_THIS_OPTIONS_ROUTER: Router,
+      })
+    )
+  
+    const Component = compose(OriginComponent)
+  
+    const testRenderer = enzyme.mount(<Component />)
+
+    const originComponent = testRenderer.find(OriginComponent)
+    originComponent.find('.searchButton').simulate('click')
+
+    expect(validateFields).toBeCalled()
+    expect(routerReplace).toBeCalled()
+    expect(routerReplace).toBeCalledWith({
+      pathname: '/trending',
+      query: {
+        p: 1,
+        pageSize: 15,
+        id: '123456',
+        updateTimeStart: 1560407706310,
+        updateTimeEnd: 1560407857997,
+      }
+    })
+    expect(dispatch).toBeCalled()
+    expect(dispatch).toBeCalledWith({
+      type: 'testDispatch/getList',
+      payload: {
+        p: 1,
+        pageSize: 15,
+        id: '123456',
+        updateTimeStart: 1560407706310,
+        updateTimeEnd: 1560407857997,
+      }
+    })
   })
 })
+
